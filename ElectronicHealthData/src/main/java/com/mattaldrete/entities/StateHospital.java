93d9164b2ded6508577a0ae16aae1004b9162ef4
Matt <mra286@humboldt.edu>
2020-04-13 17:09:17 -0600

    Convert To CSV file complete

--- ElectronicHealthData/src/main/java/com/mattaldrete/entities/StateHospital.java
+++ ElectronicHealthData/src/main/java/com/mattaldrete/entities/StateHospital.java
@@ -1,37 +1,60 @@
 package com.mattaldrete.entities;
 
 public class StateHospital {
-	
+
 	private String region;
 	private String regionCode;
 	private int period;
 	private double pct_hospitals_basic_ehr_notes;
-	
+
 	public String getRegion() {
 		return region;
 	}
+
 	public void setRegion(String region) {
 		this.region = region;
 	}
+
 	public String getRegionCode() {
 		return regionCode;
 	}
+
 	public void setRegionCode(String regionCode) {
 		this.regionCode = regionCode;
 	}
+
 	public int getPeriod() {
 		return period;
 	}
+
 	public void setPeriod(int period) {
 		this.period = period;
 	}
+
 	public double getPct_hospitals_basic_ehr_notes() {
 		return pct_hospitals_basic_ehr_notes;
 	}
+
 	public void setPct_hospitals_basic_ehr_notes(double pct_hospitals_basic_ehr_notes) {
 		this.pct_hospitals_basic_ehr_notes = pct_hospitals_basic_ehr_notes;
 	}
-	
-	
+
+	public StateHospital(String region, String regionCode, int period, double pct_hospitals_basic_ehr_notes) {
+		super();
+		this.region = region;
+		this.regionCode = regionCode;
+		this.period = period;
+		this.pct_hospitals_basic_ehr_notes = pct_hospitals_basic_ehr_notes;
+	}
+
+	public StateHospital() {
+
+	}
+
+	@Override
+	public String toString() {
+		return "StateHospital [region=" + region + ", regionCode=" + regionCode + ", period=" + period
+				+ ", pct_hospitals_basic_ehr_notes=" + pct_hospitals_basic_ehr_notes + "]\n";
+	}
 
 }
--- ElectronicHealthData/src/main/java/com/mattaldrete/json/HospitalEHR.java
+++ ElectronicHealthData/src/main/java/com/mattaldrete/json/HospitalEHR.java
@@ -1,16 +1,24 @@
 package com.mattaldrete.json;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mattaldrete.entities.StateHospital;
 import com.mattaldrete.entities.StateHospitalAllData;
 
 public class HospitalEHR {
@@ -26,13 +34,20 @@ public class HospitalEHR {
 
 		String jsonString = jsonToString(url);
 //		System.out.println(jsonString);
+
+		List<StateHospital> hospitals = jsonToJavaObject(jsonString);
+		System.out.println("2014 List size: " + hospitals.size());
+
+		List<StateHospital> hospitalsSorted = sortDescByEHC(hospitals);
+		System.out.println(hospitalsSorted);
 		
-		jsonToShAllData(jsonString);
+		
+		String fileName = "ehr.csv";
+		
+		convertToCSV(hospitalsSorted, fileName);
 
 	}
 
-	
-	
 	public static String jsonToString(URL url) {
 		String jsonString = "";
 		try {
@@ -42,9 +57,6 @@ public class HospitalEHR {
 			int responsecode = conn.getResponseCode();
 
 //			System.out.println(responsecode);
-
-			List<StateHospitalAllData> stateHospitals = new ArrayList<StateHospitalAllData>();
-
 			if (responsecode != 200) {
 				throw new RuntimeException("HttpRepsonseCode: " + responsecode);
 			} else {
@@ -58,7 +70,6 @@ public class HospitalEHR {
 //				System.out.println(jsonString);
 //				System.out.println("done");
 				sc.close();
-
 			}
 
 		} catch (MalformedURLException e) {
@@ -68,20 +79,69 @@ public class HospitalEHR {
 		}
 		return jsonString;
 	}
-	
-	
-	public static void jsonToShAllData(String jsonString) {
+
+	public static List<StateHospital> jsonToJavaObject(String jsonString) {
 		ObjectMapper mapper = new ObjectMapper();
-		
+		List<StateHospital> shList = new ArrayList<StateHospital>();
+
 		try {
+			// Data is an array of objects
 			StateHospitalAllData[] state = mapper.readValue(jsonString, StateHospitalAllData[].class);
-			
-			
-			
-			
+
+			for (int i = 0; i < state.length; i++) {
+				int period = state[i].getPeriod();
+				String regionCode = state[i].getRegionCode();
+
+				if ((period == 2014) && (!regionCode.contentEquals("US"))) {
+					StateHospital sh = new StateHospital();
+					sh.setRegion(state[i].getRegion());
+					sh.setRegionCode(state[i].getRegionCode());
+					sh.setPeriod(state[i].getPeriod());
+					sh.setPct_hospitals_basic_ehr_notes(state[i].getPct_hospitals_basic_ehr_notes());
+					shList.add(sh);
+
+				}
+			}
+
 		} catch (JsonMappingException e) {
 			e.printStackTrace();
 		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		return shList;
+	}
+
+	public static List<StateHospital> sortDescByEHC(List<StateHospital> shList) {
+		Collections.sort(shList, new Comparator<StateHospital>() {
+
+			@Override
+			public int compare(StateHospital o1, StateHospital o2) {
+				return Double.valueOf(o2.getPct_hospitals_basic_ehr_notes())
+						.compareTo(o1.getPct_hospitals_basic_ehr_notes());
+			}
+		});
+
+		return shList;
+	}
+
+	public static void convertToCSV(List<StateHospital> data, String fileName) {
+		try {
+			FileWriter fw = new FileWriter(fileName);
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter pw = new PrintWriter(bw);
+
+			pw.println("State\t" + "EHR %\t" + "Period\t");
+			for (StateHospital sh : data) {
+				pw.println(sh.getRegion() + "\t" + sh.getPct_hospitals_basic_ehr_notes() + "\t" + sh.getPeriod());
+			}
+
+			pw.flush();
+			pw.close();
+
+			JOptionPane.showMessageDialog(null, "File Created");
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
